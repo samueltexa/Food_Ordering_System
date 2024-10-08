8b2608cf42491bbb8fa967094fc0158a12302977
samueltexa <woolardsamuels@gmail.com>
2024-10-08 18:31:55 +0300

    update footer.php

--- src/components/footer.php
+++ src/components/footer.php
@@ -1,11 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Header</title>
-</head>
-<body>
-    
-</body>
-</html>
+<!-- footer.php -->
+<footer>
+<link rel="stylesheet" href="../../public/assets/css/main.css">
+    <div class="footer-container">
+        <div class="developers">
+            <h3>Developers</h3>
+            <ul>
+                <li>ALINDA SAMUEL</li>
+                <li>ADANIA KEZZY</li>
+                <li>MUTUNGI FELIX</li>
+                <li>AGUMENAITWE SIMPLICIOUS</li>
+                <li>BUDALA EDMUND TUTUMA</li>
+                <li>KEMBABAZI FANCY ORIKIRIZA</li>
+                <li>TUSIIME AGNESS</li>
+                <li>ODONG NOAH 2021/BSE/168/PS</li>
+                <li>OKEMA ELVIS ATUHAIRWE</li>
+                <li>SSEMBAJJA ASHIRAF</li>
+            </ul>
+        </div>
+        <div class="contact-info">
+            <h3>Contact Information</h3>
+            <ul>
+                <li>Contact Us</li>
+                <li>Reach Us</li>
+                <li>About Us</li>
+            </ul>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <p class="paragraph">&copy; 2024 FeastHive. All rights reserved.</p>
+    </div>
+</footer>
