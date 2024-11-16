69ebfa286a2598342a6f3def270dad8409db6c1e
samueltexa <woolardsamuels@gmail.com>
2024-11-16 03:20:41 +0300

    add footer file

--- src/components/footer.php
+++ src/components/footer.php
@@ -1,32 +1,71 @@
-<!-- footer.php -->
 <footer>
-<link rel="stylesheet" href="../../public/assets/css/main.css">
     <div class="footer-container">
         <div class="developers">
-            <h3>Developers</h3>
-            <ul>
-                <li>ALINDA SAMUEL</li>
-                <li>ADANIA KEZZY</li>
-                <li>MUTUNGI FELIX</li>
-                <li>AGUMENAITWE SIMPLICIOUS</li>
-                <li>BUDALA EDMUND TUTUMA</li>
-                <li>KEMBABAZI FANCY ORIKIRIZA</li>
-                <li>TUSIIME AGNESS</li>
-                <li>ODONG NOAH 2021/BSE/168/PS</li>
-                <li>OKEMA ELVIS ATUHAIRWE</li>
-                <li>SSEMBAJJA ASHIRAF</li>
-            </ul>
-        </div>
-        <div class="contact-info">
-            <h3>Contact Information</h3>
-            <ul>
-                <li>Contact Us</li>
-                <li>Reach Us</li>
-                <li>About Us</li>
-            </ul>
+            <h3>Meet the team</h3>
+            <div class="developers-columns">
+                <ul>
+                    <li>ALINDA SAMUEL</li>
+                    <li>ADANIA KEZZY</li>
+                    <li>MUTUNGI FELIX</li>
+                </ul>
+                <ul>
+                    <li>AGUMENAITWE SIMPLICIOUS</li>
+                    <li>BUDALA EDMUND TUTUMA</li>
+                    <li>KEMBABAZI FANCY ORIKIRIZA</li>
+                </ul>
+                <ul>
+                    <li>TUSIIME AGNESS</li>
+                    <li>ODONG NOAH</li>
+                    <li>OKEMA ELVIS ATUHAIRWE</li>
+                </ul>
+                <ul>
+                    <li>SSEMBAJJA ASHIRAF</li>
+                    <li>AGUMENAITWE SIMPLICIOUS</li>
+                </ul>
+            </div>
         </div>
     </div>
     <div class="footer-bottom">
         <p class="paragraph">&copy; 2024 FeastHive. All rights reserved.</p>
     </div>
 </footer>
+
+<style>
+    .developers-columns {
+        display: flex;
+        justify-content: space-between;
+        width: 90%;
+        height: 100px;
+        align-self: center;
+        margin: 10px;
+    }
+    .developers{
+        width: 100%;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .developers-columns ul {
+        list-style-type: none;
+        padding: 0;
+        margin: 0;
+        background-color: 'red';
+    }
+    footer {
+    background-color: #7c848d;
+    color: white;
+    padding: 20px;
+    text-align: center;
+}
+
+.footer-container {
+    display: flex;
+    justify-content: space-around;
+}
+
+.footer-bottom {
+    background-color: #afb7be;
+    padding: 10px 0;
+    margin-top: 20px;
+}
+</style>
