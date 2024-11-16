f3481e0f6306eeaae4bd0550ae0ea3a80a646f2c
samueltexa <woolardsamuels@gmail.com>
2024-11-16 04:43:06 +0300

    update index.php

--- index.php
+++ index.php
@@ -10,30 +10,8 @@
 </head>
 
 <body>
-
     <!-- Header Section -->
-    <header>
-        <div>
-            <h1 class="logo">FEASTHIVE</h1>
-        </div>
-        <nav>
-            <ul>
-                <li><a href="#">HOME</a></li>
-                <li><a href="#">MENU</a></li>
-                <li><a href="#">BLOGS</a></li>
-                <li><a href="#"><i class="fas fa-question-circle"></i> HELP</a></li>
-                <li class="dropdown">
-                    <a href="#"><i class="fas fa-user"></i> ACCOUNT<i class="fas fa-chevron-down"></i></a>
-                    <div class="dropdown-content">
-                        <a href="src/View/Login.php"><i class="fas fa-sign-in-alt"></i> Login</a>
-                        <a href="#"><i class="fas fa-user"></i> Account</a>
-                        <a href="#"><i class="fas fa-box"></i> Orders</a>
-                    </div>
-                </li>
-                <li><a href="#"><i class="fas fa-shopping-cart"></i> CART</a></li>
-            </ul>
-        </nav>
-    </header>
+    <?php include 'src/components/header.php'; ?>
 
     <!-- Hero Section -->
     <section class="hero">
@@ -45,14 +23,13 @@
                 <div class="dropdown-content">
                     <a href="#">About Us</a>
                     <a href="#">Contact us</a>
-                    <a href="#">Settings</a>
                 </div>
             </div>
             <h2>Delicious Meals Delivered To You</h2>
         </div>
         <div class="hero-content">
             <p class="paragraph">Order your favorite meals with ease and get them delivered hot and fresh!</p>
-            <a href="#" class="explore-button">Explore Menu</a>
+            <a href="src/View/menu.php" class="explore-button">Explore Menu</a>
         </div>
     </section>
 
